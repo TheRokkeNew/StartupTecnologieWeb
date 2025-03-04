0620e6fbf9ff335beae98ef38de55b3dfffebc97
CarlottaGirardi <carlotta.girardi@edu.unife.it>
2025-03-04 22:04:17 +0100

    prova

--- routes/web.php
+++ routes/web.php
@@ -8,10 +8,6 @@ use App\Http\Controllers\TransactionController;
 use App\Http\Controllers\ExportController;
 use App\Http\Controllers\SelectUserController;
 use App\Http\Controllers\DashboardController;
-<<<<<<< Updated upstream
-
-=======
->>>>>>> Stashed changes
 
 //homepage
 Route::get('/', function () {
@@ -26,10 +22,7 @@ Route::get('/login', [LoginController::class, 'showLoginForm'])->name('login');
 Route::post('/login', [LoginController::class, 'login']);
 Route::get('/register', [RegisterController::class, 'showRegistrationForm'])->name('register');
 Route::post('/register', [RegisterController::class, 'register']);
-<<<<<<< Updated upstream
 
-=======
->>>>>>> Stashed changes
 Route::get('/dashboard', [DashboardController::class, 'index'])->name('dashboard');
 
 // Altre route
