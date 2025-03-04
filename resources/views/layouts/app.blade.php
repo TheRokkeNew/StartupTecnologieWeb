16023a8436f78518f6322c1c88b3db9012f7e66d
CarlottaGirardi <carlotta.girardi@edu.unife.it>
2025-03-04 12:07:42 +0100

    togli

--- resources/views/layouts/app.blade.php
+++ resources/views/layouts/app.blade.php
@@ -34,19 +34,6 @@
                 <button class="navbar-toggler" type="button" data-bs-toggle="collapse" data-bs-target="#navbarNav">
                     <span class="navbar-toggler-icon"></span>
                 </button>
-                <div class="collapse navbar-collapse" id="navbarNav">
-                    <ul class="navbar-nav ms-auto">
-                        <li class="nav-item">
-                            <a class="nav-link" href="#">Dashboard</a>
-                        </li>
-                        <li class="nav-item">
-                            <a class="nav-link" href="#">Profilo</a>
-                        </li>
-                        <li class="nav-item">
-                            <a class="nav-link btn btn-outline-danger" href="#">Logout</a>
-                        </li>
-                    </ul>
-                </div>
             </div>
         </nav>
         
